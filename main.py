61b5dfae9d77b5cb8ec05c965e01eb885d325bdf
sandeepbandi924 <bandisandeep1374@gmail.com>
2025-01-22 18:18:47 +0530

    trainer evaulation and mlflow

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig,
+                                                  DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
 import sys
 
 
@@ -28,6 +30,13 @@ if __name__ == '__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_tranformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     
     except Exception as e:
         raise NetworkSecurityException(e,sys)
